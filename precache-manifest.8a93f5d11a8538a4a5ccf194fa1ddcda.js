84780a43d01c60551357bdfdb4d2f7cb1deee3d8
cristovoxdgm <cristovoxdgm@gmail.com>
2019-11-21 11:21:12 -0300

    Updates

---
+++ precache-manifest.8a93f5d11a8538a4a5ccf194fa1ddcda.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "28dcd852b794a9124c78732b07094fa4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a9831f9f3342a291bd6d",
+    "url": "/static/css/2.833dd627.chunk.css"
+  },
+  {
+    "revision": "524b66fd159b2d7780b1",
+    "url": "/static/css/main.81ef548d.chunk.css"
+  },
+  {
+    "revision": "a9831f9f3342a291bd6d",
+    "url": "/static/js/2.fe70949a.chunk.js"
+  },
+  {
+    "revision": "524b66fd159b2d7780b1",
+    "url": "/static/js/main.6bffefe5.chunk.js"
+  },
+  {
+    "revision": "77786c3f19d499504077",
+    "url": "/static/js/runtime-main.c8d01c9b.js"
+  },
+  {
+    "revision": "08065a1a6e6bddcb69460186c036af38",
+    "url": "/static/media/logo.08065a1a.jpg"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
